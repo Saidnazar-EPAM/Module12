e1ecca1ba831919fd9df9302884e082f50f92fb4
Saidnazar-EPAM <102347678+Saidnazar-EPAM@users.noreply.github.com>
2022-04-19 10:11:46 +0500

    Task completed

---
+++ Module12/Script.PostDeployment.sql
@@ -0,0 +1,40 @@
+﻿USE [Module12]
+GO
+
+IF NOT EXISTS (SELECT 1 FROM dbo.Person)
+BEGIN
+INSERT [dbo].[Person] ([FirstName], [LastName]) VALUES 
+(N'Saidnazar', N'Saidnazarov'),
+(N'Zokir', N'O''ktamov'),
+(N'Shodlik', N'Saidnazarov'),
+(N'Bahrom', N'Ikromov')
+END
+GO
+
+IF NOT EXISTS (SELECT 1 FROM dbo.Address)
+BEGIN
+INSERT [dbo].[Address] ([Street], [City], [State], [ZipCode]) VALUES 
+(N'Oydin', N'Khiva', N'Khorezm', N'220900'),															
+(N'K.Sapayev', N'Khiva', N'Khorezm', N'220800'),
+(N'Firdavs', N'Urgench', N'Khorezm', N'200800')
+END
+GO
+
+IF NOT EXISTS (SELECT 1 FROM dbo.Employee)
+BEGIN
+INSERT [dbo].[Employee] ([AddressId], [PersonId], [CompanyName], [Position], [EmployeeName]) VALUES 
+(1, 1, N'Power', N'Designer', N'Saidnazarov Saidnazar'),
+(2, 2, N'Qwerty', N'Developer', NULL),
+(1, 3, N'Power', N'Developer', N'Shodlik Saidnazarov'),
+(3, 4, N'Ency', N'QA Engineer', NULL)
+END
+GO
+
+IF NOT EXISTS (SELECT 1 FROM dbo.Company)
+BEGIN
+INSERT [dbo].[Company] ([Name], [AddressId]) VALUES 
+(N'Power', 1),
+(N'Qwerty', 2)
+END
+GO
+
---
+++ Module12/Views/EmployeeInfo.sql
@@ -0,0 +1,7 @@
+﻿CREATE VIEW [dbo].[EmployeeInfo]
+	AS 
+	SELECT dbo.Employee.Id AS EmployeeId, CASE WHEN dbo.Employee.EmployeeName IS NULL THEN isnull(dbo.Person.FirstName, '') + ' ' + isnull(dbo.Person.LastName, '') ELSE EmployeeName END AS EmployeeFullName, 
+                  dbo.Address.ZipCode + '_' + dbo.Address.State + ', ' + dbo.Address.City + '-' + dbo.Address.Street AS EmployeeFullAddress, dbo.Employee.CompanyName + '(' + dbo.Employee.Position + ')' AS EmployeeCompanyInfo
+    FROM   dbo.Employee INNER JOIN
+                  dbo.Person ON dbo.Employee.PersonId = dbo.Person.Id INNER JOIN
+                  dbo.Address ON dbo.Employee.AddressId = dbo.Address.Id
